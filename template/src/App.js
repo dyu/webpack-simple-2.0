e8a7af864f35f5f9fe612d675ce5f69b08529b4c
dyu <david.yu.ftw@gmail.com>
2016-06-20 23:30:59 +0800

    fix msg

--- template/src/App.js
+++ template/src/App.js
@@ -2,18 +2,16 @@ import Vue from 'vue'
 import Foo from './Foo'
 
 export default Vue.extend({
-  props: ['baz'],
-  data() {
+  data () {
     return {
-      greet: 'baz!'
+      msg: 'Hello Vue 2.0!'
     }
   },
   render(h) {
     return (
       <div>
-        <foo></foo>
-        <img src="./assets/logo.png">
-        <h1>{{ msg }}</h1>
+        <h1>{ this.msg }</h1>
+        <Foo />
       </div>
     )
   }
